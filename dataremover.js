ce9ac088feaca09d4e67baaf8eb6c9a0dccad8e3
Hiren Joshi <hirenj@gmail.com>
2018-05-08 15:35:39 +0200

    Fix for data cleanup routine

--- dataremover.js
+++ dataremover.js
@@ -92,7 +92,7 @@ const delete_items = function(items) {
   let params = { 'RequestItems' : {},
                   ReturnConsumedCapacity: 'TOTAL',
                 };
-  console.log('Doing delete request with',items.length,'items for dataset ',items.map( item => item.dataset ).filter(onlyUnique));
+  console.log('Doing delete request with',items.length,'items for dataset ',items.map( item => item.dataset ).filter(onlyUnique).join(','));
   params.RequestItems[data_table] = items.map( item => { return { DeleteRequest: { Key: item } } });
   console.log("Waiting to write delete request");
   return write_limiter().then( () => {
@@ -119,21 +119,25 @@ const handle_delete = function(items_to_delete,result) {
   }
 };
 
-const remove_single_set_entries = function(dataset) {
+const remove_multiple_set_entries = function(datasets) {
   let params = {
     TableName: data_table,
-    FilterExpression: 'dataset = :dataset and NOT acc = :acc',
-    ProjectionExpression: 'acc',
+    FilterExpression: 'NOT acc = :acc',
+    ProjectionExpression: 'acc,dataset',
     ReturnConsumedCapacity: 'TOTAL',
     ExpressionAttributeValues: {
-      ':acc': 'metadata',
-      ':dataset': dataset
+      ':acc': 'metadata'
     }
   };
   if (last_scan_key) {
     params.ExclusiveStartKey = last_scan_key;
   }
-  console.log('Removing',dataset);
+  console.log('Removing',datasets.join(','));
+
+  if (datasets.length == 0) {
+    return Promise.resolve();
+  }
+
   let handle_scan = function(data) {
     console.log("Handling scan results");
     if (timed_out) {
@@ -141,7 +145,7 @@ const remove_single_set_entries = function(dataset) {
       console.log("Stopping scan retrieval");
       throw new Error('Timed out');
     }
-    let items_to_delete = data.Items.map( item => { return { acc: item.acc, dataset: dataset }; } );
+    let items_to_delete = data.Items.filter( item => datasets.indexOf(item.dataset) >= 0 ).map( item => { return { acc: item.acc, dataset: item.dataset }; } );
     let new_rate_per_10s = Math.floor(10*MAX_READ_CAPACITY / data.ConsumedCapacity.CapacityUnits);
     if (read_limiter.rate_per_10s !== new_rate_per_10s) {
       console.log("Limiting to",new_rate_per_10s,"calls per 10 seconds");
@@ -151,7 +155,7 @@ const remove_single_set_entries = function(dataset) {
     // console.log('Scan / Read capacity',data.ConsumedCapacity.CapacityUnits);
     // console.log('Items returned after scan',data.Items.length);
 
-    console.log('Need to remove',items_to_delete.length,'items from current scan on',dataset);
+    console.log('Need to remove',items_to_delete.length,'items from current scan on',datasets.join(','));
     console.log("Sending delete request");
     let delete_done = delete_items(items_to_delete.splice(0,25)).then( handle_delete.bind(null,items_to_delete));
 
@@ -192,12 +196,11 @@ const remove_single_set_metadata = function(dataset) {
   });
 };
 
-const remove_single_set = function(dataset) {
-  current_dataset = dataset;
-  if ( ! dataset ) {
+const remove_multiple_sets = function(datasets) {
+  if ( ! datasets ) {
     return;
   }
-  return remove_single_set_entries(dataset).then( remove_single_set_metadata.bind(null,dataset) );
+  return remove_multiple_set_entries(datasets).then( () => { return loop_promise( remove_single_set_metadata, datasets ); });
 };
 
 //get_datasets_to_remove().then( sets => { sets = ['glycodomain_glycodomain_10090'].concat(sets); console.log(sets); return remove_single_set(sets[0]);  }).catch(err => console.error(err));
@@ -213,24 +216,37 @@ const loop_promise = function(func,items) {
 const remove_sets_with_timeout = function(last_status) {
   if (last_status && last_status.dataset) {
     last_scan_key = last_status.last_scan_key;
-    current_dataset = last_status.dataset;
+    current_dataset = last_status.dataset.split(',');
   }
   let deleter_promise = null;
+
+  let all_sets = [];
 
   let timeout_promise = new TimeoutPromise( execution_timeout, (resolve,reject) => {
     deleter_promise = get_datasets_to_remove().then( sets => {
       if (current_dataset) {
-        sets = [current_dataset].concat(sets);
+        sets = current_dataset.concat(sets);
       }
-      return loop_promise(remove_single_set,sets.filter(onlyUnique));
+      // all_sets.length = 0;
+      sets.filter(onlyUnique).forEach( set => {
+        if (all_sets.indexOf(set) < 0) {
+          all_sets.push(set);
+        }
+      });
+      console.log('We will need to remove ',all_sets.join(','));
+      return loop_promise(remove_multiple_sets,[ [].concat(all_sets) ]);
     });
     deleter_promise.then(resolve).catch(reject);
   });
   return timeout_promise.catch( err => {
+    if (err.message !== 'Timed out') {
+      throw err;
+      return;
+    }
     console.log("Timed out with error",err);
     timed_out = true;
     let current_status = {
-      dataset: current_dataset,
+      dataset: all_sets.join(','),
       last_scan_key: last_scan_key,
       messageCount: 1,
       status: 'RUNNING'
@@ -246,95 +262,10 @@ const remove_sets_with_timeout = function(last_status) {
   });
 };
 
+
 const datasetCleanup = function(event,context) {
 
-// const sets_to_cleanup = [
-// 'glycoproteome_081cho_t10ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt10',
-// 'glycoproteome_081cho_t11ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt11',
-// 'glycoproteome_081cho_t1ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt1',
-// 'glycoproteome_081cho_t1kot2ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt1Galnt2',
-// 'glycoproteome_081cho_t2ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt2',
-// 'glycoproteome_081cho_t3ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt3',
-// 'glycoproteome_081cho_t7ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt7',
-// 'glycoproteome_081cho_t7kot10ko_CHOGSbto0000457_WTC1galt1c1KOC1galt1c1Galnt7Galnt10',
-// 'glycoproteome_081drosophila_7227bto0001489',
-// 'glycoproteome_081drosophila_ogako_7227bto0001489_KOOga',
-// 'glycoproteome_081hacat_sc_HaCaTbto0000552_KOC1GALT1C1',
-// 'glycoproteome_081hacat_t1ko_HaCaTbto0000552_WTC1GALT1C1KOC1GALT1C1GALNT1',
-// 'glycoproteome_081hacat_t2ko_HaCaTbto0000552_WTC1GALT1C1KOC1GALT1C1GALNT2',
-// 'glycoproteome_081hacat_t3ko_HaCaTbto0000552_WTC1GALT1C1KOC1GALT1C1GALNT3',
-// 'glycoproteome_081hek293_pomt1kopomt2ko_HEK293bto0000007_KOC1GALT1C1POMT1POMT2POMGNT1',
-// 'glycoproteome_081hek293_stt3bko_HEK293bto0000007_KOSTT3B',
-// 'glycoproteome_081hek293_t10ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT10',
-// 'glycoproteome_081hek293_t11ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT11',
-// 'glycoproteome_081hek293_t1ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT1',
-// 'glycoproteome_081hek293_t2ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT2',
-// 'glycoproteome_081hek293_t3ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT3',
-// 'glycoproteome_081hek293_t7ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT7',
-// 'glycoproteome_081hek293_t7kot10ko_HEK293bto0000007_WTC1GALT1C1KOC1GALT1C1GALNT7GALNT10',
-// 'glycoproteome_081hela_erk8_HeLabto0000567_KOMAPK15',
-// 'glycoproteome_081hela_erk8_HeLabto0000567_WTC1GALT1C1KOMAPK15',
-// 'glycoproteome_081hepg2_t2partial_HepG2bto0000599_KOGALNT2',
-// 'glycoproteome_081human_prostate_9606bto0001129_KOC1GALT1C1GALNT1OtherCancer',
-// 'glycoproteome_081huvec_HUVECCbto0001949',
-// 'glycoproteome_081ls174_t3ko_LS174Tbto0001553_WTC1GALT1C1KOC1GALT1C1GALNT3',
-// 'glycoproteome_081ls174_t3kot6ko_LS174Tbto0001553_WTC1GALT1C1KOC1GALT1C1GALNT3GALNT6',
-// 'glycoproteome_081ls174_t3kovst6ko_LS174Tbto0001553_WTC1GALT1C1GALNT3KOC1GALT1C1GALNT6',
-// 'glycoproteome_081ls174_t6ko_LS174Tbto0001553_WTC1GALT1C1KOC1GALT1C1GALNT6',
-// 'glycoproteome_081pig_braindec2015_9823bto0000142',
-// 'glycoproteome_081pig_braindec2015_9823bto0000232',
-// 'glycoproteome_081pig_heart_9823bto0000862',
-// 'glycoproteome_081pig_heart_9823bto0000903',
-// 'glycoproteome_081pig_pancreas_9823bto0000988',
-// 'glycoproteome_081plasma_9606bto0000131',
-// 'glycoproteome_081platelet_9606bto0000132',
-// 'glycoproteome_081sc_CHOGSbto0000457_KOC1galt1c1',
-// 'glycoproteome_081t1_HepG2bto0000599_KOC1GALT1C1GALNT1',
-// 'glycoproteome_081t2_HepG2bto0000599_KOC1GALT1C1GALNT2',
-// 'glycoproteome_081t3ki_HepG2bto0000599_KIGALNT3KOC1GALT1C1',
-// 'glycoproteome_081viral_msf_9606pustule_OtherVZVinfection',
-// 'glycoproteome_081viral_msf_HELFbto0003034_OtherHCMVTowneinfection',
-// 'glycoproteome_081viral_msf_HELFbto0003034_OtherVZVinfection',
-// 'glycoproteome_081viral_msf_HaCaTbto0000552_OtherHSV2infection',
-// 'glycoproteome_081viral_msf_P3HR1bto0000164_OtherEBVinfection',
-// 'glycoproteome_081wt_CHOGSbto0000457',
-// 'glycoproteome_10090Muscle_Phospho',
-// 'glycoproteome_10090Muscle_WGA',
-// 'glycoproteome_7227',
-// 'glycoproteome_7227_KOOga',
-// 'glycoproteome_9606Plasma',
-// 'glycoproteome_9606Platelet',
-// 'glycoproteome_9606whole',
-// 'glycoproteome_9606whole_OtherVZVinfection',
-// 'glycoproteome_9823Brain',
-// 'glycoproteome_9823Heartatrial',
-// 'glycoproteome_9823Heartventricle',
-// 'glycoproteome_9823Pancreas',
-// 'glycoproteome_CHO_GS',
-// 'glycoproteome_CHO_GSKOC1galt1c1',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt1',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt10',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt11',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt1Galnt2',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt2',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt3',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt7',
-// 'glycoproteome_CHO_GSKOC1galt1c1Galnt7Galnt10',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT1',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT10',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT11',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT2',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT3',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT7',
-// 'glycoproteome_HEK293_KOC1GALT1C1GALNT7GALNT10',
-// 'glycoproteome_HEK293_KOC1GALT1C1POMT1POMT2POMGNT1'
-// ];
 
-
-  console.log("Dataset cleanup disabled until it is running faster");
-  context.succeed({status: 'OK', messageCount: 0 });
-
-  return;
 
   remove_sets_with_timeout(event).then( message => {
     console.log("Suceeding with",message);
@@ -342,7 +273,7 @@ const datasetCleanup = function(event,context) {
   })
   .catch( err => {
     console.log("Failed with",err);
-    context.fail({status: err.message});
+    context.fail({status: 'ERROR', message: err.message});
   });
 };
 
